3b137b0a454df72fff5f755cb35ac52aed1e3f50
WilliamsC50 <williams.m.cody@gmail.com>
2026-03-29 10:23:49 -0400

    Adjust CarouselPanel height for improved layout consistency

--- app/(display)/copperrocket/CarouselPanel.tsx
+++ app/(display)/copperrocket/CarouselPanel.tsx
@@ -209,7 +209,7 @@ export default function CarouselPanel() {
           <div
             style={{
               width: "100%",
-              height: 480,
+              height: 320,
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
